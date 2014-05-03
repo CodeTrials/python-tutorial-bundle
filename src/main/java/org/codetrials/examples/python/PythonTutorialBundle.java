b66129506300f6d9383b55acd0e4fb33aada03cc
PolarNick <PolarHare@gmail.com>
2014-05-03 12:27:15 +0400

    About function added

--- src/main/java/org/codetrials/examples/python/PythonTutorialBundle.java
+++ src/main/java/org/codetrials/examples/python/PythonTutorialBundle.java
@@ -2,10 +2,11 @@ package org.codetrials.examples.python;
 
 import org.codetrials.bundle.Task;
 import org.codetrials.bundle.engines.PythonEngine;
+import org.codetrials.bundle.entities.ExecutionResult;
 import org.codetrials.bundle.entities.TaskDescription;
+import org.codetrials.bundle.entities.TaskReaction;
 import org.codetrials.bundle.helpers.SimpleBundleContainer;
 import org.codetrials.bundle.helpers.tasks.FreeTask;
-import org.codetrials.bundle.helpers.tasks.MultipleRegexpTask;
 import org.codetrials.bundle.helpers.tasks.SandboxTask;
 
 import java.io.IOException;
@@ -29,7 +30,25 @@ public class PythonTutorialBundle extends SimpleBundleContainer {
     protected List<Task> createTasks() {
         try {
             Task task1 = new FreeTask(load(0), "next");
-            Task task2 = new MultipleRegexpTask(load(1), "Try \"help\" or \"credits\"!", "go on", "(help|copyright|credits|license)");
+            Task task2 = new Task(load(1)) {
+
+                boolean completed = false;
+
+                @Override
+                public boolean isCompleted() {
+                    return completed;
+                }
+
+                @Override
+                public TaskReaction onCommandExecuted(String s, ExecutionResult executionResult) {
+                    if (s.equals("about")) {
+                        completed = true;
+                        return new TaskReaction("Made at JetBrains EdTech Hackathon, May 1st-3rd, 2014 in St. Petersburg, Russia");
+                    } else {
+                        return new TaskReaction(null);
+                    }
+                }
+            };
             Task task3 = new FreeTask(load(2), "next");
             Task task4 = new SandboxTask(load(3));
             ArrayList<Task> list = new ArrayList<>();
